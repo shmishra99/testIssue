168bfdf2c4e1ed0fcc576d3c2bf398302f18d141
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-10-27 14:37:16 +0530

    Create auto-assignment.js

---
+++ .github/workflows/script/auto-assignment.js
@@ -0,0 +1,29 @@
+const CONSTENT_VALUES = require('./constant');
+
+module.exports = async ({ github, context }) =>  {
+    const issueNumber = context.payload.issue.number;
+    const assigneesList = ['kuaashish'];
+    console.log("assignee list",assigneesList)
+    console.log("entered auto assignment for this issue:  ", issueNumber);
+    if (!assigneesList.length) {
+      console.log('No assignees found for this repo.');
+      return;
+    }
+    let noOfAssignees = assigneesList.length;
+    let selection = issueNumber % noOfAssignees;
+    let assigneeForIssue = assigneesList[selection]
+
+      console.log(
+        'issue Number =', issueNumber + ', assigning issue to:',
+        assigneeForIssue);
+      return github.rest.issues.addAssignees({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        assignees:[assigneeForIssue] 
+
+    });
+
+  }
+
+   
